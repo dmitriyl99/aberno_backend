0e9adc2cb916f01519fdd143879a3cbb84332b9d
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-02-02 11:46:08 +0500

    Restructure token generating

--- app/core/services/auth.py
+++ app/core/services/auth.py
@@ -1,4 +1,6 @@
 from app.settings import settings
+from datetime import timedelta, datetime, timezone
+
 from jose import jwt, JWTError
 
 
@@ -9,3 +11,12 @@ class JWTAuthService:
         sub_data: str = payload.get('sub')
 
         return sub_data
+
+    @staticmethod
+    def generate_token(subject: dict):
+        to_encode = subject.copy()
+        expires_delta: timedelta = timedelta(minutes=settings.access_token_expires_minutes)
+        expire = datetime.now(timezone.utc) + expires_delta
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.jwt_algorithm)
+        return encoded_jwt
--- app/use_cases/auth/create_token_use_case.py
+++ app/use_cases/auth/create_token_use_case.py
@@ -2,17 +2,15 @@ from typing import Annotated
 
 from fastapi import Depends
 from app.dal.models.auth.user import User
-from app.tasks.auth.create_jwt_token_task import CreateJWTTokenTask
+from app.core.services.auth import JWTAuthService
 from passlib.context import CryptContext
 
 
 class CreateTokenUseCase:
     pwd_context: CryptContext
-    create_jwt_token_task: CreateJWTTokenTask
 
-    def __init__(self, create_jwt_token_task: Annotated[CreateJWTTokenTask, Depends(CreateJWTTokenTask)]):
+    def __init__(self):
         self.pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
-        self.create_jwt_token_task = create_jwt_token_task
 
     async def execute(self, phone: str, password: str) -> dict | None:
         user = await User.filter(phone=phone).first()
@@ -21,7 +19,7 @@ class CreateTokenUseCase:
         password_verified = self.pwd_context.verify(password, user.password)
         if not password_verified:
             return None
-        access_token = self.create_jwt_token_task.run(data={"sub": str(user.id)})
+        access_token = JWTAuthService.generate_token(subject={"sub": str(user.id)})
 
         return {
             'access_token': access_token,

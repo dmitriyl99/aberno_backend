c96a02090879b897ac8c3d144823f0b60a1445c1
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-05-21 19:53:21 +0500

    fix cors

--- app/main.py
+++ app/main.py
@@ -30,13 +30,13 @@ origins = [
     "http://localhost:5173"
 ]
 
-app.add_middleware(CORSMiddleware(
-    app=app,
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-))
+)
 
 app.include_router(router=authentication.router, prefix='/api/auth')
 for router in organization.routers:

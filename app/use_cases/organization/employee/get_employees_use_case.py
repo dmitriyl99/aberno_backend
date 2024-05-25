7e87e7fba1f45deacbe2c320ffafc07244b2cccb
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-05-25 17:16:11 +0500

    fixes for search and status

--- app/use_cases/organization/employee/get_employees_use_case.py
+++ app/use_cases/organization/employee/get_employees_use_case.py
@@ -46,12 +46,14 @@ class GetEmployeesUseCase:
             )
             if search is not None:
                 query = query.filter(or_(
-                    User.name.ilike(f"%{search}%"),
+                    Employee.user.has(User.name.ilike(f"%{search}%")),
+                    Employee.user.has(User.last_name.ilike(f"%{search}")),
                     Employee.phone.ilike(f"%{search}%"),
                 ))
             if department_id is not None:
                 query = query.filter(Employee.department_id == department_id)
             if employee_status:
-                is_active_filter = employee_status == 'active'
+                is_active_filter = employee_status == 'true'
+                print(is_active_filter)
                 query = query.filter(Employee.user.has(User.is_active == is_active_filter))
             return query.order_by(Employee.created_at.desc()).limit(per_page).offset((page - 1) * per_page).all(), query.count()

fc4e5ff1903d5322ae78b29a41d0cc9089315883
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-06-23 13:19:17 +0500

    Add new route for employee list

--- app/routers/organization/employee.py
+++ app/routers/organization/employee.py
@@ -3,11 +3,40 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.core.facades.auth import Auth
+from app.routers.admin.view_models import EmployeeResponse
 from app.tasks.organization.get_current_employee_task import GetCurrentEmployeeTask
 from app.dependencies import verify_authenticated_user
-
+from app.use_cases.organization.employee import GetEmployeesUseCase
 
 router = APIRouter(prefix='/employee', dependencies=[Depends(verify_authenticated_user)])
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_employees(
+        get_employees_use_case: Annotated[GetEmployeesUseCase, Depends(GetEmployeesUseCase)],
+        search: str | None = None,
+        department_id: int | None = None,
+        position_id: int | None = None,
+        status: str | None = None,
+        page: int = 1,
+        per_page: int = 10
+):
+    employees, count = get_employees_use_case.execute(
+        Auth.get_current_user(),
+        search,
+        department_id,
+        position_id,
+        status,
+        page,
+        per_page
+    )
+
+    return {
+        'count': count,
+        'data': list(
+            map(lambda employee: EmployeeResponse.from_model(employee), employees)
+        )
+    }
 
 
 @router.get('/current/')
